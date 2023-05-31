0a8a61484d2072f0a24bba00755bad6c8021371d
Bobby Tyrone Woody <bobbywoody7@gmail.com>
2023-05-31 12:11:25 -0600

    update list item and page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,12 +7,23 @@ export default function Home() {
       <Header>
         {" "}
         <div className="mb-2">
-          <h1 className="text-white text-3xl font-semibold">Welcome bacj</h1>
+          <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-            <ListItem />
+            <ListItem
+              image="/images/liked.png"
+              name="Liked Songs"
+              href="liked"
+            />
           </div>
         </div>
       </Header>
+      <div className="my-2 mb-7 px-6">
+        {" "}
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
+        </div>
+        <div>List of Songs!</div>
+      </div>
     </main>
   );
 }
