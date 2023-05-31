1774c7fedb2b15e6c92331262ec718d6f585d4a8
Bobby Tyrone Woody <bobbywoody7@gmail.com>
2023-05-31 11:44:04 -0600

    create a library component

---
+++ components/Library.tsx
@@ -0,0 +1,29 @@
+import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
+
+const Library = () => {
+  const onClick = () => {
+    // todo handle upload
+  };
+  return (
+    <div className=" flex flex-col">
+      {" "}
+      <div className="flex items-center justify-between px-5 pt-4">
+        {" "}
+        <div className="inline-flex  items-center gap-x-2">
+          {" "}
+          <TbPlaylist size={24} className="text-netural-400" />
+          <p className="text-neutral-400 font-medium text-md">Your Library</p>
+        </div>{" "}
+        <AiOutlinePlus
+          onClick={onClick}
+          size={24}
+          className="text-neutral-400 cursor-pointer hover:text-white transition"
+        />
+      </div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3"> List of songs!</div>
+    </div>
+  );
+};
+
+export default Library;
