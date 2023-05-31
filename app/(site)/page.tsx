869296c777af184aaac63479f54dbf8197280dcc
Bobby Tyrone Woody <bobbywoody7@gmail.com>
2023-05-30 23:01:00 -0600

    permissions changed

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="text-cyan-400">hello soundvibe</main>;
+  return <main className="text-cyan-400">Main Content</main>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
-import { Figtree, Inter } from "next/font/google";
+import { Figtree } from "next/font/google";
+import Sidebar from "@/components/Sidebar";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Sidebar>{children}</Sidebar>
+      </body>
     </html>
   );
 }
