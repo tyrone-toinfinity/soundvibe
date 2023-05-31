d230afb2bd104d5a2f28acca8c7a80955b051955
Bobby Tyrone Woody <bobbywoody7@gmail.com>
2023-05-31 11:35:16 -0600

    updated page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,18 @@
-import Image from "next/image";
+import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
-  return <main className="text-cyan-400">Main Content</main>;
+  return (
+    <main className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        {" "}
+        <div className="mb-2">
+          <h1 className="text-white text-3xl font-semibold">Welcome bacj</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+            <ListItem />
+          </div>
+        </div>
+      </Header>
+    </main>
+  );
 }
